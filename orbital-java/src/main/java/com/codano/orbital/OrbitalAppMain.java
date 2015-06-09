363c69cd01b54bcb58f3a9d1d6e60375317658cd
Matt Mastracci <matthew@mastracci.com>
2015-06-08 18:54:32 -0600

    Boot the given main class

--- orbital-java/src/main/java/com/codano/orbital/OrbitalAppMain.java
+++ orbital-java/src/main/java/com/codano/orbital/OrbitalAppMain.java
@@ -1,7 +1,31 @@
 package com.codano.orbital;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 public class OrbitalAppMain {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		String main = args[0];
+		System.out.println("Booting main: " + main);
+
+		Thread t = new Thread(() -> invokeMain(main));
+		t.setName("main");
+		t.start();
+	}
+
+	private static void invokeMain(String main) {
+		try {
+			Class<?> mainClass = Class.forName(main);
+			for (Method method : mainClass.getDeclaredMethods()) {
+				if (method.getName().equals("main")
+						&& ((method.getModifiers() & Modifier.STATIC) != 0)) {
+					// TODO: would be nice to pass process.argv from node.js
+					// here
+					method.invoke(null, new Object[0]);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
